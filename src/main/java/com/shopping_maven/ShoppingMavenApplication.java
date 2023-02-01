3ce96e0e59875ca9ef07fd86c12ed56d2c1d3915
zongbeen <hjb825@naver.com>
2023-02-01 22:58:25 +0900

    Initial commit

---
+++ src/main/java/com/shopping_maven/ShoppingMavenApplication.java
@@ -0,0 +1,13 @@
+package com.shopping_maven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingMavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingMavenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shopping_maven/ShoppingMavenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shopping_maven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingMavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
